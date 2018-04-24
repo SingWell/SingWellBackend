7ee720b598f0af37b40495427b0d9bb0033bf16c
popov95s <spopov@smu.edu>
2018-04-24 16:29:39 -0500

    Added arranger to parse script and created youtube link resource

--- api/parse_library.py
+++ api/parse_library.py
@@ -5,6 +5,11 @@ def parse_library(library_file, organization_id):
     df = pd.read_excel(library_file, 'Sheet1', index_col=None)
     for index,row in df.iterrows():
         if(row['TITLE']!=None):
-            mrecord, created = MusicRecord.objects.get_or_create(organization_id= organization_id, title= row['TITLE'],composer=row['COMPOSER'], publisher=row['PUBLISHER'], instrumentation=row['SCORING'])
-            mrecord.save()
+            try:
+                mrecord, created = MusicRecord.objects.get_or_create(organization_id= organization_id, title= row['TITLE'],composer=row['COMPOSER'], publisher=row['PUBLISHER'], arranger=row['ARRANGER'], instrumentation=row['INSTRUMENTATION'])
+                mrecord.save()
+                text_resource, created = TextResource.objects.get_or_create(title = row['TITLE'], music_record = mrecord, type='youtube_link', field=row['YOUTUBE LINK'], description=None)
+                text_resource.save()
+            except: 
+                return False
     return True
